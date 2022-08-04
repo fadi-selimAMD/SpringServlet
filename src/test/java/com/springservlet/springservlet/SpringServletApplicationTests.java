e6397aa80ad74b410d0310d57be0a18dadc3cf05
Fadi <fadi.selim@allegiancemd.com>
2022-08-04 14:17:58 +0200

    Initial commit

---
+++ src/test/java/com/springservlet/springservlet/SpringServletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springservlet.springservlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringServletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
